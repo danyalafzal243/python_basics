81d950ffbb96debf20b98b2cf9e840fd1738fcbd
Danyal Afzal <Danyal@test.com>
2020-08-11 10:48:01 -0700

    python introduction 1

---
+++ python_introduction_1.py
@@ -0,0 +1,26 @@
+
+print('Hello world')
+
+# ============================ variables ==================== 
+cup = 'Coffee'
+print( cup )
+
+# python is cases sensitive 
+# A , a (Not equal)
+# danyal, DANYAL (Not equal)
+
+name_of_city = "Peshwar"
+print(name_of_city)
+
+nameofcity = 'lahore'
+print(nameofcity)
+
+
+#  ******* Python Statement, Indentation and Comments ********
+# Python Indentation
+
+for x in range(1, 11):
+    print(x)
+
+# Python Comments
+# '#' for single line and multi line
