42baed620ac888bbfdd2f15291e71fc8f9790013
Danyal Afzal <danyalafzal243@gmail.com>
2020-08-14 10:50:07 -0700

    day 2

--- python_introduction_2.py
+++ python_introduction_2.py
@@ -1,23 +1,57 @@
+# =============== Python Variables, Constants and Literals =================
 
-print('Hello world')
+number = 100000
+print(number)
 
-# ============================ variables ==================== 
-cup = 'Coffee'
-print( cup )
+number = 1.1
+print(number)
 
-# python is cases sensitive 
-# A , a (Not equal)
-# danyal, DANYAL (Not equal)
 
-name_of_city = "Peshwar"
-print(name_of_city)
+# Example 2: Changing the value of a variable
+college_name = 'Lundkhwarh'
+print(college_name)
 
-#  ******* Python Statement, Indentation and Comments ********
-# Python Indentation
+college_name = 'Mardan'
+print(college_name) 
 
-for x in range(1, 11):
-    print(x)
+# Example 3: Assigning multiple values to multiple variables
+student_name, college_name, degree = 'Ali', "Mardan", "Bs"
+print(student_name, college_name, degree)
 
-# Python Comments
-# '#' for single line and multi line
+
+# ------------------ Literals ------------------
+# Numeric
+unit = 12
+price = 56.67
+
+print("Unit => ", type(unit), "price => ", type(price))
+
+# String literals
+phrase = 'Example 7: How to use string literals in Python?'
+char = 'c'
+
+print("phrase => ", type(phrase), "char => ", type(char))
+
+# Boolean literals
+is_raining = False
+is_winter = True
+
+print("is_raining => ", type(is_raining), "is_winter => ", type(is_winter))
+
+# Literal Collections
+customers = ["Ali", "Ali", "Jawad", "Faisal"] # list
+facts = ('LITTER', 'BARREL') # tuple
+vowels = {'a', 'e', 'i' , 'o', 'u'} #set
+map = {
+    'name': "Saeed khan",
+    'age': 32,
+    'cell': "0302020206"
+} # dictionary
+
+
+print(type(customers))
+print(type(facts))
+print(type(vowels))
+print(type(map))
+
 
