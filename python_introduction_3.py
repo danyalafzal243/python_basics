e55d6aac0b7051ebcb1046f3f70c9c85aaeeaf7f
Danyal Afzal <danyalafzal243@gmail.com>
2020-08-18 11:16:04 -0700

    day 3

---
+++ python_introduction_3.py
@@ -0,0 +1,56 @@
+# ===================== Python Numbers ========================
+
+no_of_students = 45
+print(type(no_of_students)) #? 
+
+price_of_drink = 45.78
+print(type(price_of_drink)) #?
+ 
+type(no_of_students) #?
+
+result_sum = no_of_students + price_of_drink #?
+type(result_sum) #?
+
+
+# ========================== string =======================
+# A - Z
+# a -z
+# 1, 9
+# special character !@#$%^&*()`,./`
+
+phrase = "We love pakistan" #? 
+length_of_phrase = len(phrase) #?
+
+character = phrase[5] #?
+type(phrase) #?
+
+first_name = "Danyal "
+last_name = "Afzal"
+
+full_name = first_name +  last_name #?
+
+unit = "5"
+ballance = "6.22"
+
+sum = unit + ballance #?
+print(type(unit)) 
+print(type(ballance)) 
+
+#  ============== Conversion =========================
+converted_unit = int( "35" ) 
+converted_ballance = float( "66.27" )
+print(type(converted_unit)) 
+print(type(converted_ballance)) 
+
+converted_sum = converted_unit + converted_ballance #?
+
+# ========================= Lists ==============================
+study_group = ["Danyal", "Mohsin", "Zohaib", "Ihtisham", "Bilal", 88, 55.99]
+
+study_group_length = len(study_group) #?
+val = study_group[0] #?
+val = study_group[1] #?
+val = study_group[2] #?
+val = study_group[3] #?
+val = study_group[4] #?
+
