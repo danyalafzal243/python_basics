c258e28dfe727c6456eb6abd7cbfb5dc8563958e
Danyal Afzal <danyalafzal243@gmail.com>
2020-08-19 11:07:20 -0700

    day 4

---
+++ python_introduction_4.py
@@ -0,0 +1,17 @@
+dicionary  = { "age": 34, "name": "failsal" }
+type(dicionary) #?
+
+
+# =================== input and output ===============
+name = input("Enter your name ? ")
+
+print("Hi, "+ name)
+print(f"Hi, {name}")
+
+
+# ===================== import =================
+import math
+
+print(math.floor(344.65)) #?
+print(math.ceil(344.15)) #?
+
