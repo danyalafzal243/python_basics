966e0160845d62c270cca8478f2d4c7c81ee1ec1
Danyal Afzal <danyalafzal243@gmail.com>
2020-08-20 11:13:43 -0700

    day 5

---
+++ python_introduction_5.py
@@ -0,0 +1,74 @@
+# =============== Arithmetic Operators =============
+sum = 5 + 3 #?
+sub = 15 - 4 #?
+mul = 5 * 4  #20 #?
+div = 25 / 4 # 6.25 #?
+
+mod = 25 % 4 # 1 #?
+mod = 35 % 12 # 11 #?
+
+floor_div = 10 // 3 #?
+floor_div = 25 // 4 #?
+
+exp = 3 ** 3 # (3 * 3 * 3) #?
+exp = 2 ** 4 # (2 * 2 * 2 * 2)  #?
+
+# =============== Comparison Operators ( True / False ) =============
+
+# Greater Than >
+gt = 15 > 10 #?
+gt = 7 > 33 #?
+gt = 45 > 45 #?
+
+# Greater Than Eqaul to >=
+gt = 15 >= 10 #?
+gt = 7 >= 33 #?
+gt = 45 >= 45 #?
+
+# Less Than <
+lt = 15 < 10 #?
+lt = 7 < 33 #?
+lt = 45 < 45 #?
+
+# Less Than equal to <=
+lt = 15 <= 10 #?
+lt = 7 <= 33 #?
+lt = 45 <= 45 #?
+
+# Equality ==
+eqt = 15 == 15 #?
+eqt = 15 == 20 #?
+
+eqt = 15 != 15 #?
+eqt = 15 != 20 #?
+
+# =================== Logical operators ( True / False )=================
+and_operator = True and True # True #?
+and_operator = True and False # False #?
+and_operator = False and False # False #?
+
+or_operator = True or True # True #?
+or_operator = True or False # False #?
+or_operator = False or False # False #?
+
+
+and_ex = (10 == 10) and (5 == 5) and (40 == 40) #?
+and_ex = (10 == 10) and (5 == 15) #?
+and_ex = (105 == 10) and (50 == 15) #?
+ 
+or_ex = (10 == 10) or (5 == 5) or (40 == 40) #?
+or_ex = (5000 == 15) or (34 == 1675) or (10 == 10) or (5 == 15) #?
+or_ex = (105 == 10) or (50 == 15) #?
+
+ok = True
+not_ok = False
+
+print(ok) # True #?
+print(not ok) #False #?
+
+print(not_ok) # False #?
+print(not not_ok) #True #?
+
+
+
+
